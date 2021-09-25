0a40f575361029c6b93f9dca5c8fa515ff81d888
Yuvindu <it20101374@my.sliit.lk>
2021-09-25 13:08:48 +0530

    Added dish post

---
+++ app/src/main/java/com/example/pizza_mad/chefFoodPanel/Chef.java
@@ -0,0 +1,80 @@
+package com.example.pizza_mad.chefFoodPanel;
+
+public class Chef {
+
+    private String Area, City, ConfirmPassword, Emailid, Fname, House, Lname, Mobile, Password, Postcode, District;
+
+    // Press Alt+Insert
+
+
+    public Chef(String area, String city, String confirmPassword, String emailid, String fname, String house, String lname, String mobile, String password, String postcode, String district) {
+        this.Area = area;
+        City = city;
+        ConfirmPassword = confirmPassword;
+        Emailid = emailid;
+        Fname = fname;
+        House = house;
+        Lname = lname;
+        Mobile = mobile;
+        Password = password;
+        Postcode = postcode;
+        District = district;
+    }
+    public Chef() {
+    }
+
+    public String getArea() {
+
+        return Area;
+    }
+
+    public String getCity() {
+
+        return City;
+    }
+
+    public String getConfirmPassword() {
+
+        return ConfirmPassword;
+    }
+
+    public String getEmailid() {
+
+        return Emailid;
+    }
+
+    public String getFname() {
+
+        return Fname;
+    }
+
+    public String getHouse() {
+
+        return House;
+    }
+
+    public String getLname() {
+
+        return Lname;
+    }
+
+    public String getMobile() {
+
+        return Mobile;
+    }
+
+    public String getPassword() {
+
+        return Password;
+    }
+
+    public String getPostcode() {
+
+        return Postcode;
+    }
+
+    public String getDistrict() {
+
+        return District;
+    }
+}
